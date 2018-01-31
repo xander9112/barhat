7cba20f934564375914c776f5f8b30b4c6bb5fe1
AlexZander <xander91@mail.ru>
2018-01-31 23:18:41 +0300

    fix

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,6 +1,7 @@
 import Tabs from './Components/Tabs'
 import OwlCarousel from './Components/OwlCarousel'
 import MainNav from './Components/MainNav'
+import Projects from './Components/Projects'
 
 import 'jquery-validation'
 import './plugins/jquery.formstyler'
@@ -11,8 +12,6 @@ import './plugins/jquery.formstyler'
 export default class Application {
   constructor () {
     this._initComponents()
-    //    TODO: УДАЛИТЬ!!!!
-    this._initAssets()
 
     $('input, select').styler()
 
@@ -36,22 +35,53 @@ export default class Application {
       min: jQuery.validator.format('Please enter a value greater than or equal to {0}.')
     })
 
-    $('.js-form').validate({
-      errorClass: 'error',
-      rules: {
-        'email': {
-          required: true,
-          email: true
+    $('.js-form').each((index, element) => {
+      const formObject = $(element)
+
+      formObject.validate({
+        errorClass: 'error',
+        rules: {
+          'email': {
+            required: true,
+            email: true
+          },
+          'name': 'required',
+          'phone': 'required'
         },
-        'name': 'required'
-      },
 
-      submitHandler: (form, event) => {
-        console.log(form, event)
+        showErrors: function (errorMap, errorList) {
+          if (this.numberOfInvalids()) {
+            formObject.find('.js-error-content').html('Некорректные данные')
+          } else {
+            formObject.find('.js-error-content').html(' ')
+          }
+          this.defaultShowErrors()
+        },
 
-        event.preventDefault()
-        return false
-      }
+        highlight: function (element, errorClass) {
+          $(element).parent().addClass('form-group_error')
+          $(element).addClass('error')
+          /* $(element).fadeOut(function() {
+             $(element).fadeIn();
+           });*/
+        },
+
+        unhighlight: function (element, errorClass) {
+          $(element).parent().removeClass('form-group_error')
+          $(element).removeClass('error')
+          /* $(element).fadeOut(function() {
+             $(element).fadeIn();
+           });*/
+        },
+
+        submitHandler: (form, event) => {
+          event.preventDefault()
+
+          $(form).find('.js-error-content').html(' ')
+
+          return false
+        }
+      })
     })
 
     $('.js-tabs').each((index, element) => {
@@ -65,6 +95,23 @@ export default class Application {
     })
 
     new MainNav($('.js-main-nav'))
+
+    $('.js-reviews').each((index, element) => {
+      const wrap = $(element).parents('.js-reviews-wrap')
+
+      const owlCarousel = $(element).owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        items: 1
+      })
+
+      wrap.on('click', '.js-reviews-next', (event) => {
+        event.preventDefault()
+
+        owlCarousel.trigger('next.owl.carousel')
+      })
+    })
 
     $('.js-component').each((index, element) => {
       const componentName = $(element).data('component')
@@ -82,13 +129,33 @@ export default class Application {
         let component = new componentClass.default($(element), options)
       }
     })
+
+    $('.js-projects').each((index, element) => {
+      new Projects($(element))
+    })
+
+    $(window).on('resize', () => {
+      setTimeout(() => {
+        this.centeredImages()
+      }, 100)
+    })
+
+    $(window).trigger('resize')
   }
 
-  _initAssets () {
-    if (location.host === '192.168.0.150') {
-      $('html').find('[rel="stylesheet"]').attr('href', `./dist/app.css?time=${+new Date()}`)
-    }
+  centeredImages () {
+    if ($('.js-center-image').length) {
+      $('.js-center-image').each((index, element) => {
+        const image = $(element)
+        const parent = image.parent()
+        const marginLeft = image.width() > parent.width() ? (image.width() - parent.width()) / 2 : 0
 
-    //    $('.main-slider__footer-title').html($(window).width())
+        image.css({
+          width: 'auto',
+          float: 'left',
+          marginLeft: `-${marginLeft}px`
+        })
+      })
+    }
   }
 }
--- src/js/Components/MainNav.js
+++ src/js/Components/MainNav.js
@@ -8,11 +8,15 @@ export default class MainNav extends Component {
   get _defaultOptions () {
     'use strict'
 
-    return {}
+    return {
+      offsetTop: $('.js-slider-logo').length ? $('.js-slider-logo').offset().top : 50
+    }
   }
 
   initialize () {
     'use strict'
+
+    this.width = document.body.clientWidth
 
     super.initialize()
   }
@@ -21,21 +25,30 @@ export default class MainNav extends Component {
     'use strict'
 
     this.nodes = {
-      toggle: this.root.find('.js-toggle')
+      nav: this.root.find('.js-nav'),
+      item: this.root.find('.js-nav li'),
+      itemLink: this.root.find('.js-nav li a'),
+      toggle: this.root.find('.js-toggle'),
+
+      toggleItem: this.root.find('.js-toggle-item')
     }
   }
 
   _bindEvents () {
-    'use strict'
-
     this.nodes.toggle.on('click', (event) => {
       event.preventDefault()
 
       this.toggle()
     })
 
+    $(window).bind('resize orientationchange', () => {
+      this.width = document.body.clientWidth
+
+      this.adjustMenu()
+    })
+
     $(window).on('scroll', () => {
-      if ($(window).scrollTop() > 50) {
+      if ($(window).scrollTop() > this.options.offsetTop) {
         this.root.addClass('main-nav_fixed')
       } else {
         this.root.removeClass('main-nav_fixed')
@@ -52,6 +65,7 @@ export default class MainNav extends Component {
   }
 
   toggle () {
+    console.log(this.root.hasClass('main-nav_opened'))
     if (this.root.hasClass('main-nav_opened')) {
       this.close()
     } else {
@@ -59,9 +73,32 @@ export default class MainNav extends Component {
     }
   }
 
+  adjustMenu () {
+    if (this.width < 768) {
+      this.nodes.item.unbind('mouseenter mouseleave')
+
+      this.nodes.itemLink.filter('.parent').unbind('click').bind('click', event => {
+        event.preventDefault()
+        $(event.currentTarget).parent('li').toggleClass('hover')
+      })
+    } else if (this.width >= 768) {
+      this.nodes.item.removeClass('hover')
+      this.nodes.itemLink.unbind('click')
+      this.nodes.item.unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function (event) {
+        $(event.currentTarget).toggleClass('hover')
+      })
+    }
+  }
+
   _ready () {
     'use strict'
 
-    //    this.root.owlCarousel(this.options)
+    this.nodes.itemLink.each(function () {
+      if ($(this).next().length > 0) {
+        $(this).addClass('parent')
+      }
+    })
+
+    this.adjustMenu()
   }
 }
